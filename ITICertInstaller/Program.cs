2d3fe9d6200d9c200b4594f56d2a3153109a069f
Fernando Ribeiro da Silva <fernando.ribeiro@gmail.com>
2018-05-23 20:54:03 -0300

    Installing ITI and MPF certs with success.

--- ITICertInstaller/Program.cs
+++ ITICertInstaller/Program.cs
@@ -4,39 +4,129 @@ using System.IO;
 using System.IO.Compression;
 using System.Security.Cryptography.X509Certificates;
 
-namespace ITICertInstaller
+namespace WinCertInstaller
 {
     class Program
     {
+        static Stream DownloadFile(String url)
+        {
+            WebClient client = new WebClient();
+            Stream stream = null;
+            try
+            {
+                stream = client.OpenRead(url);
+            } catch (System.Net.WebException ex)
+            {
+                Console.WriteLine("ERROR: Unable to download certificates.");
+                Console.WriteLine("ERROR: {0}", ex.Message);
+            }
+
+            return stream;
+        }
+
+        static X509Certificate2Collection GetITICertificates()
+        {
+            X509Certificate2Collection certCollection = new X509Certificate2Collection();
+            String url = "http://acraiz.icpbrasil.gov.br/credenciadas/CertificadosAC-ICP-Brasil/ACcompactado.zip";
+            Console.WriteLine("ITI: Getting certificates from {0} please wait.", url);
+            Stream stream = DownloadFile(url);
+
+            if (stream != null) {
+                ZipArchive archive = new ZipArchive(stream);
+
+                foreach (ZipArchiveEntry certificate in archive.Entries)
+                {
+                    Stream certStrean = certificate.Open();
+                    MemoryStream ms = new MemoryStream();
+                    X509Certificate2 cert = new X509Certificate2();
+                    certStrean.CopyTo(ms);
+                    cert.Import(ms.ToArray());
+                    certCollection.Add(cert);
+                }
+                Console.WriteLine("ITI: {0} certificates found.", certCollection.Count);
+            }
+            return certCollection;
+        }
+
+        static X509Certificate2Collection GetMPFCertificates()
+        {
+            X509Certificate2Collection certCollection = new X509Certificate2Collection();
+            String url = "http://repositorio.acinterna.mpf.mp.br/ejbca/downloads/ACIMPF-cadeia-completa.p7b";
+            Console.WriteLine("MPF: Getting certificates from {0} please wait.", url);
+            Stream stream = DownloadFile(url);
+            if (stream != null) { 
+                MemoryStream ms = new MemoryStream();
+                stream.CopyTo(ms);
+                certCollection.Import(ms.ToArray());
+                Console.WriteLine("MPF: {0} certificates found.", certCollection.Count);
+            }
+            return certCollection;
+        }
+
+        static void InstallCertificates(String name, X509Store store, X509Certificate2Collection certificates) {
+            try
+            {
+                if (certificates.Count > 0)
+                {
+                    Console.WriteLine("{0}: Installing certificates.", name);
+                    store.AddRange(certificates);
+                    Console.WriteLine("{0}: Added {1} certificates to {2}.", name, certificates.Count, StoreName.Root);
+                } else
+                {
+                    Console.WriteLine("{0}: No certificates to import.", name);
+                }
+            }
+            catch (System.Security.Cryptography.CryptographicException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+                System.Environment.Exit(-1);
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            String url = "http://acraiz.icpbrasil.gov.br/credenciadas/CertificadosAC-ICP-Brasil/ACcompactado.zip";
-            WebClient client = new WebClient();
-            Stream zipCertificateStream = client.OpenRead(url);
-            ZipArchive archive = new ZipArchive(zipCertificateStream);
+            bool quiet = false;
+            if(args.Length > 0)
+            {
+                if (args[0] == "-q")
+                {
+                    quiet = true;
+                } else
+                {
+                    Console.WriteLine("Wrong parameter. Aborting.");
+                    Console.WriteLine("Valid parameters are only: -q");
+                    System.Environment.Exit(-1);
+                }
+            }
 
             X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
             store.Open(OpenFlags.MaxAllowed);
 
-
-            foreach (ZipArchiveEntry certificate in archive.Entries)
-            {
-                Stream certStrean = certificate.Open();
-                X509Certificate2 cert = new X509Certificate2();
-                MemoryStream ms = new MemoryStream();
-                certStrean.CopyTo(ms);
-                cert.Import(ms.ToArray());
-                Console.WriteLine(cert.Subject);
-                try
-                {
-                    store.Add(cert);
-                } catch (System.Security.Cryptography.CryptographicException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    System.Environment.Exit(-1);
-                }
+            Console.WriteLine("====================== ITI ======================");
+            X509Certificate2Collection ITIcertificates = GetITICertificates();
+            if (ITIcertificates.Count > 0) {
+                InstallCertificates("ITI", store, ITIcertificates);
             }
+
+            Console.WriteLine("====================== MPF ======================");
+            X509Certificate2Collection MPFCertificates = GetMPFCertificates();
+            if (MPFCertificates.Count > 0)
+            {
+                InstallCertificates("MPF", store, MPFCertificates);
+            }
+            
             store.Close();
+            Console.WriteLine("=================================================");
+            Console.WriteLine("Finished!");
+
+            if (!quiet)
+            {
+                Console.WriteLine("To run in quiet mode use -q parameters.");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey(true);
+            }
+            
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,17 @@
+# WinCertInstaller
+
+This repository contains a c# application which install ITI and MPF certificates in Windows Trusted Root Store
+
+*Needs Administrator privileges to run.*
+
+*Use at your own risk.*
+
+## TODO
+
+It makes sense to reconstruct a minimally-working original code
+
+What is missing right now:
+* Better exceptions catch
+
+
+*Feel free to make pull-requests :)*
